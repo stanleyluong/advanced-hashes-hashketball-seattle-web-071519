c6b4130005295f3e96c7cb7dce81d24351321b0e
Stanley Luong <slluong@uw.edu>
2019-07-12 17:53:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -146,11 +146,11 @@ def num_points_scored(name)
   end
 end
 
-def shoe_size(name)
+def shoe_size#(name)
   hash = game_hash
   hash.each do |location, info| 
     info.each do |attribute, stuff| 
-      if stuff.include?(name) 
+      # if stuff.include?(name) 
        return hash[location][attribute][name][:shoe]
       end
     end
