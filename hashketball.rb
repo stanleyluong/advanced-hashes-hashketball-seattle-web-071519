b24e01763becdd1347f040be33b9f70c4e4512ba
Stanley Luong <slluong@uw.edu>
2019-07-12 17:56:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -241,7 +241,17 @@ def big_shoe_rebounds
   end
 end
 
- 
+def most_points_scored(game)
+  max_player = nil
+  game.each do |team, team_hash|
+    team_hash[:players].each do |player, player_hash|
+      max_player ||= player_hash
+      max_player = player_hash if player_hash[:stats][:points] > max_player[:stats][:points]
+    end
+  end
+
+  max_player[:name]
+end
 
 
 
